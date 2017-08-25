9c07f0200918090b0b6df8020ccb14b306c295ff
chetanyachopra <devchetanyachopra@gmail.com>
2017-08-25 23:47:47 +0530

    training transformed

--- classifier.py
+++ classifier.py
@@ -2,36 +2,22 @@ import pandas as pd
 import pprint
 import numpy as np
 
+#make a dataframe
 df = pd.read_table('dataset/SMSSpamCollection', sep ='\t',  header =None, names = ['label', 'message'])
 
+#label features with 0 and 1
 df['label'] = df.label.map({'ham':0, 'spam':1})
 
-#print(df.shape[0])
-# print(df.head())
-
-
-documents = ['Hello, how are you!',
-                'Win money, win from home.',
-                'Call me now.',
-                'Hello, Call hello you tomorrow?']
-
-from sklearn.feature_extraction.text import CountVectorizer
-count_vector = CountVectorizer()
-count_vector.fit(documents)
-#pprint.pprint(count_vector.get_feature_names())
-
-doc_arr = count_vector.transform(documents).toarray()
-#print(doc_arr)
-
-doc_arr_df = pd.DataFrame(doc_arr, columns = count_vector.get_feature_names())
-#print(doc_arr_df)
-
+#dividing in training and testing data
 from sklearn.model_selection import train_test_split as tts
 
 x_message_train, x_message_test, y_label_train, y_label_test = tts(df['message'], df['label'], random_state = 1)
 
-print('total rows = '+format(df.shape[0]))
-print('total rows in training = '+ format(x_message_train.shape[0]))
-print('total rows in testing = ' + format(x_message_test.shape[0]))
+#implimenting bag of words
+from sklearn.feature_extraction.text import CountVectorizer
+count_vector = CountVectorizer()
 
-#another change
+training_data = count_vector.fit_transform(x_message_train)
+testing_data = count_vector.transform(x_message_test)
+
+print(eval(str(count_vector.get_feature_names())))
